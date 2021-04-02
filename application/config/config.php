635d67b53b0d552fa8b944974e16ccbb4d2982dd
Joca <jperez.477@jpit.com.mx>
2021-04-02 10:09:13 -0600

    cambio2

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mercadopagovintage/';
-//$config['base_url'] = 'https://doritienda-mp.herokuapp.com/';
+//$config['base_url'] = 'http://localhost/mercadopagovintage/';
+ $config['base_url'] = 'https://tiendavintage.herokuapp.com/';
 
 
 /*
